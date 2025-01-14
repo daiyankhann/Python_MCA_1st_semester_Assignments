d85924d743e219bebd9e8109bd10b3cc1115f802
daiyankhann <daiyanhudakhan@gmail.com>
2025-01-14 16:55:51 +0530

    Add files via upload

---
+++ Assignment 4/Odd.py
@@ -0,0 +1,7 @@
+def odd(num):
+    if num%2==0:
+        print(f"{num} is even number")
+    else:
+     print(f"{num} is odd number")
+n= int (input("Enter a number to check ODD or EVEN : ",))
+odd(n)
---
+++ Assignment 4/Square.py
@@ -0,0 +1,5 @@
+def square(n):
+    s=n*n
+    return s
+a = int(input("Enter a number : "))
+print(f"Square of {a} is :",square(a))
